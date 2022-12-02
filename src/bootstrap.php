55c339f940eb1dbcf3603f389d3e55ce43dc9562
Samuele Carafassi <samuele.carafassi@studio.unibo.it>
2022-12-02 20:31:30 +0100

    added utility functions

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,6 +3,7 @@ session_start();
 define('UPLOAD_DIR', '../imgs/uploads/');
 
 require_once('../db/DatabaseHelper.php');
+require_once('functions.util.php');
 
 $dbh = new DatabaseHelper('localhost', 'root', '', 'social_network');
 ?>
