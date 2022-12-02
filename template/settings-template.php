6c23e5a6195899d8e545b15b0592feb00302cbd1
Guo jiahao <jiahao.guo@studio.unibo.it>
2022-12-02 22:39:46 +0100

    added css for alternate column bg color in setting's page

--- template/settings-template.php
+++ template/settings-template.php
@@ -1,13 +1,13 @@
-<main class="text-left">
+
     <div class="row">
         <div class="col-md-2"></div>
         <div class="col-12 col-md-8">
             <section class="bg-white border border-primary" style="height: 100vh;">
                 <div class="card col-10 col-md-8 mx-auto">
                     <nav class="navbar">
-                        <ul class="navbar-nav w-100">
+                        <ul class="navbar-nav w-100" id="settings-ul">
                             <li class="nav-item ">
-                                <a class="nav-link" href="#" style="background-color: #7c8286;">
+                                <a class="nav-link" href="#">
                                     <h2>Profile Settings</h2>
                                 </a>
                             </li>
@@ -17,7 +17,7 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#" style="background-color: #7c8286;">
+                                <a class="nav-link" href="#">
                                     <h2>Account Management</h2>
                                 </a>
                             </li>
@@ -26,9 +26,13 @@
                                     <h2>Privacy & Policy</h2>
                                 </a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <h2>Logout</h2>
+                                </a>
+                            </li>
                         </ul>
                     </nav>
                 </div>
             </section>
             <div class="col-md-2"></div>
-</main>
