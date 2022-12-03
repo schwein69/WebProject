e9a989f100c29a9a46226db3b8b014ef03aa8430
Samuele Carafassi <samuele.carafassi@studio.unibo.it>
2022-12-03 16:11:39 +0100

    changed title size and position

--- template/base.php
+++ template/base.php
@@ -16,16 +16,23 @@
         <header>
             <div class="row">
                 <div class="col-12 col-md-8 mx-auto">
-                    <nav class="navbar navbar-expand-md navbar-light" style="background-color: #49acf3;">
-                        <h1><a class="navbar-brand position-absolute top-0 start-50 translate-middle-x"
-                                href="#">Lynkzone</a></h1>
-                        <ul class="nav nav-pills ms-auto">
-                            <li class="mx-1"> <button type="button" class="btn btn-light"><span
-                                        class="bi bi-plus-circle"></span></button></li>
-                            <li class="mx-1"> <button type="button" class="btn btn-light"><span class="bi bi-bell"></span></button>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div class="container-fluid" style="background-color: #49acf3;">
+                    <div class="row">
+                        <div class="col-6 col-md-7 my-auto text-end">
+                        <h1><a href="#">Lynkzone</a></h1>
+                        </div>
+                        <div class="col-6 col-md-5">
+                        <nav class="navbar navbar-expand-md navbar-light">
+                            <ul class="nav nav-pills ms-auto">
+                                <li class="mx-1"> <button type="button" class="btn btn-light"><span
+                                            class="bi bi-plus-circle"></span></button></li>
+                                <li class="mx-1"> <button type="button" class="btn btn-light"><span class="bi bi-bell"></span></button>
+                                </li>
+                            </ul>
+                        </nav>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </header>
