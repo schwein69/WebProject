d38be0c0d57187d114202db60ca668e73ef34124
Guo jiahao <jiahao.guo@studio.unibo.it>
2023-01-21 15:41:54 +0100

    changed id to class for follow and save button, fixed a bug in follow.js, fixed wrong parameters (when calling isUserFOlloedbyMe) in setting.php, added some js in setting.php( for management in savedPost page), optimized when showing follow button(disabled in profile page of other users because the is already a follow button in the top)

--- js/follow-event.js
+++ js/follow-event.js
@@ -1,20 +1,20 @@
-delegate_event('click', document, 'button[id^=follower]', follow);
+delegate_event('click', document, "button[class*=followButton]", follow);
 function follow(event) {
     event.preventDefault();
     const source = event.target || event.srcElement;
     const followButton = source.nodeName.toLowerCase() == 'button' ? source : source.parentNode;
-    const userId = followButton.id.substring(8);
+    const userId = followButton.value;
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
         console.log(this.responseText);
         const response = JSON.parse(this.responseText);
-        const contentPlace = document.querySelectorAll("button[id=follower" + userId + "]");//tutti i button che hanno lo stesso id
+        const contentPlace = document.querySelectorAll("button[class*=followButton" + userId + "]");//tutti i button che hanno lo stesso id
         if (response.follower) {//quelli che seguo io
             contentPlace.forEach(element => {//aggiorna la scritta a tutti i post dello stesso utente
                 element.innerText = element.innerText === "Follow" ? "Followed" : "Seguito";
             });
-            if (document.querySelector(".card-body ul li:last-child a") != null && document.querySelector(".card-body ul li:last-child a").nextSibling != null) {//se lo seguo dal profilo aggiorno i numeri
-                let updatePlace = document.querySelector(".card-body ul li:last-child a").nextSibling;
+            if (document.querySelector(".card-body ul li:nth-child(3) a") != null && document.querySelector(".card-body ul li:nth-child(3) a").nextSibling != null) {//se lo seguo dal profilo aggiorno i numeri
+                let updatePlace = document.querySelector(".card-body ul li:nth-child(3) a").nextSibling;
                 let value = parseInt(updatePlace.nodeValue) + 1;
                 updatePlace.nodeValue = value;
 
@@ -23,8 +23,8 @@ function follow(event) {
             contentPlace.forEach(element => {
                 element.innerText = element.innerText === "Followed" ? "Follow" : "Segui";
             });
-            if (document.querySelector(".card-body ul li:last-child a") != null && document.querySelector(".card-body ul li:last-child a").nextSibling != null) {
-                let updatePlace = document.querySelector(".card-body ul li:last-child a").nextSibling;
+            if (document.querySelector(".card-body ul li:nth-child(3) a") != null && document.querySelector(".card-body ul li:nth-child(3) a").nextSibling != null) {
+                let updatePlace = document.querySelector(".card-body ul li:nth-child(3) a").nextSibling;
                 let value = parseInt(updatePlace.nodeValue) - 1;
                 updatePlace.nodeValue = value;
             }
